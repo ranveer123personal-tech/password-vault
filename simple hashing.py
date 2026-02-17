3ec93204bf29612b7a7bbc264cd7643b19a74805
ranveer123personal-tech <ranveer123personal@gmail.com>
2026-02-17 09:50:20 +0530

    code to save the master password as hash

--- simple hashing.py
+++ simple hashing.py
@@ -1,6 +1,23 @@
 import hashlib
+import os
 
-converter=hashlib.sha256()
-a=input("enter password")
-converter.update(a.encode())
-print(converter.digest())
+modify=hashlib.sha3_512()
+
+password=input("enter the top secret password")
+modify.update(password.encode())
+converted_words=modify.hexdigest()
+try:
+    parent='E:/'
+    file_to_make='top_secret'
+    path=os.path.join(parent,file_to_make)
+    os.mkdir(path)
+except FileNotFoundError as e:
+    print('folder not found:', e)
+except PermissionError as e:
+    print('access denied:', e)
+except Exception as e:
+    print('something unexpected happened:', e)
+save_file=open(f'{path}/hashed.txt','w+')
+save_file.write(f'date-2026-03-15{converted_words}')
+print(save_file.read())
+save_file.close()
